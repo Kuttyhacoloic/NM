d04de5a76bbfd8f81fa8441264e00f7cfafd6b35
GOWTHAM <100853284+Kuttyhacoloic@users.noreply.github.com>
2023-10-11 14:47:25 +0530

    Add files via upload
    File uploaded

---
+++ naan.py
@@ -0,0 +1,44 @@
+ximport numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+# Load the credit card transaction dataset 
+data = pd.read_csv('creditcard.csv')
+
+# Split the dataset into features (X) and the target variable (y)
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Train a logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+
+# Print the evaluation metrics
+print(f'Accuracy: {accuracy:.2f}')
+print(f'Precision: {precision:.2f}')
+print(f'Recall: {recall:.2f}')
+print(f'F1 Score: {f1:.2f}')
+print(f'Confusion Matrix:\n{conf_matrix}')
+print(f'Classification Report:\n{classification_rep}')
---
+++ vi.py
@@ -0,0 +1,20 @@
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+# Load the credit card transaction dataset
+data = pd.read_csv('creditcard.csv')
+
+# Data preprocessing and splitting
+
+# Standardize the features
+
+# Train a logistic regression model
+
+# Make predictions on the test set
+
+# Evaluate the model
